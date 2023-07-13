5705c270e5fd74f139fd6b17c36f2b0c8f9ad76d
Chu Yulim <20102124@g.seoultech.ac.kr>
2023-07-14 00:57:54 +0900

    firebase setting

--- app/src/main/java/com/app/veggie/calPage/calPage.kt
+++ app/src/main/java/com/app/veggie/calPage/calPage.kt
@@ -8,6 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.app.veggie.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.fragment_cal_page.*
 import kotlinx.android.synthetic.main.fragment_main_page.*
 import kotlinx.android.synthetic.main.tool_bar.*
@@ -15,19 +20,46 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 class calPage : Fragment() {
+    // 파이어베이스 세팅 - 스토리지
+    private val storage: FirebaseStorage =
+        FirebaseStorage.getInstance("gs://mp-project-2023.appspot.com/")
+    private val storageRef: StorageReference = storage.reference
+
+    // 파이어베이스 세팅 - 파이어스토어
+    val firestore = Firebase.firestore
+    val auth = FirebaseAuth.getInstance()
 
     var todaysDate = "00.00"
 
+    lateinit var testText: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        todaysDate = todayDate();
+        todaysDate = todayDate()
+
+        firestore.collection("TB_TEST").document("test_text").get()
+            .addOnSuccessListener { document ->
+                if (document["contents"] != null){
+                    testText = document["contents"] as String
+                    firebaseText.text = testText
+                }else{
+                    testText = "데이터 없음"
+                    firebaseText.text = testText
+                }
+
+            }.addOnFailureListener {
+                testText = "연결안됨"
+                firebaseText.text = testText
+            }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_cal_page, container, false)
+        val view =  inflater.inflate(R.layout.fragment_cal_page, container, false)
+
+        return view
     }
 
     @SuppressLint("SetTextI18n")
