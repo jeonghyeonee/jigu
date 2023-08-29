f6552340be745fa54a221a83c5780b33320580fc
Chu Yulim <20102124@g.seoultech.ac.kr>
2023-08-29 13:16:49 +0900

    add marker

---
+++ app/src/main/java/data/Rest.kt
@@ -0,0 +1,21 @@
+package data
+
+import ted.gun0912.clustering.clustering.TedClusterItem
+import ted.gun0912.clustering.geometry.TedLatLng
+import java.io.Serializable
+
+data class Rest (
+    val REST_ID: Long,
+    val REST_NAME: String,
+    val REST_CATEGORY: String,
+    val REST_TEL: String?,
+    val REST_SI: String?,
+    val REST_GU: String?,
+    val REST_DONG: String?,
+    val REST_LAT: Double,
+    val REST_LNG: Double
+): Serializable, TedClusterItem {
+    override fun getTedLatLng(): TedLatLng {
+        return TedLatLng(REST_LAT, REST_LNG)
+    }
+}
