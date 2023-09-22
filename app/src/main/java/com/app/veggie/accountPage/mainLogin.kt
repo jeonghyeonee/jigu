b5df141b6653b91091628877839f0cf7fbb4b500
Chu Yulim <20102124@g.seoultech.ac.kr>
2023-09-22 14:03:45 +0900

    add splash view

---
+++ app/src/main/java/com/app/veggie/accountPage/mainLogin.kt
@@ -0,0 +1,14 @@
+package com.app.veggie.accountPage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.veggie.R
+
+class mainLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_login)
+    }
+
+
+}
